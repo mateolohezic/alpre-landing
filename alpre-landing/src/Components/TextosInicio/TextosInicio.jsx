240b61101f72f1d64b292bad375291dd3ff3a86b
Mateo Lohezic <mateolohezic@gmail.com>
2023-06-23 12:58:37 -0300

    update imagen caracteristicas

--- alpre-landing/src/Components/TextosInicio/TextosInicio.jsx
+++ alpre-landing/src/Components/TextosInicio/TextosInicio.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './textosInicio.css'
 import icono from '../../assets/icono.svg'
+import imagen from '../../assets/CaracteristicasInicio.png'
 
 function TextosInicio() {
   return (
@@ -9,8 +10,11 @@ function TextosInicio() {
         <div className='textoInicio1'>
           <p>MAS DE 20 AÑOS DEDICADOS A LA CONSTRUCCIÓN DE OBRAS DE ARQUITECTURA E INGENIERÍA PARA EL SECTOR PÚBLICO E INDUSTRIAL</p>
         </div>
-        <div className='textoInicio2'>
+        {/* <div className='textoInicio2'>
           <p>ARQUITECTURA <span>|</span> INGENIERÍA CIVIL <span>|</span> ELECTROMECÁNICA <span>|</span> METALÚRGICA</p>
+        </div> */}
+        <div className='imagenInicio2'>
+          <img src={imagen} alt="Características Alpre" />
         </div>
         <div className='textoInicio3'>
           <p>Compromiso con la excelencia, la rigurosidad técnica y el cumplimiento de los más altos estándares de calidad y plazos asumidos</p>
