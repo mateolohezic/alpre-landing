31db69b152b891d89ad6815a5303b39416b6dc28
mateolohezic <mateolohezic@gmail.com>
2024-02-07 11:08:25 -0300

    New: lazy component#

--- src/Components/ServiciosObras/ServiciosObras.jsx
+++ src/Components/ServiciosObras/ServiciosObras.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import './serviciosObras.css'
+import { LazyLoadComponent } from 'react-lazy-load-image-component';
 import CarouselServicio from '../CarouselServicio/CarouselServicio'
 import arquitectura1 from '../../assets/Arquitectura/1.webp'
 import arquitectura2 from '../../assets/Arquitectura/2.webp'
@@ -53,76 +54,84 @@ function ServiciosObras() {
       <div className='separacionTituloCategoria'></div>
       <div className='contenedorServicios'>
         <div className='tituloServiciosyObras' id="Servicios">SERVICIOS &amp; OBRAS</div>
-        <div className='itemServicio' id='arquitecturaServicio'>
-          <div className='infoItemServicio'>
-            <div className='textoItemServicio'>
-              <h2>ARQUITECTURA</h2>
-              <ul>
-                <li>Construcción de barrios de viviendas.</li>
-                <li>Escuelas y hospitales.</li>
-                <li>Edificios de viviendas y comerciales de distintos niveles.</li>
-                <li>Complejos turísticos residenciales.</li>
-                <li>Estructuras de hormigón elaborado.</li>
-              </ul>
+        <LazyLoadComponent>
+          <div className='itemServicio' id='arquitecturaServicio'>
+            <div className='infoItemServicio'>
+              <div className='textoItemServicio'>
+                <h2>ARQUITECTURA</h2>
+                <ul>
+                  <li>Construcción de barrios de viviendas.</li>
+                  <li>Escuelas y hospitales.</li>
+                  <li>Edificios de viviendas y comerciales de distintos niveles.</li>
+                  <li>Complejos turísticos residenciales.</li>
+                  <li>Estructuras de hormigón elaborado.</li>
+                </ul>
+              </div>
+              <CarouselServicio imagenes={imgArquitecturaXL} id='carouselArquitecturaXL' desktop/>
             </div>
-            <CarouselServicio imagenes={imgArquitecturaXL} id='carouselArquitecturaXL' desktop/>
+            <CarouselServicio imagenes={imgArquitectura} id='carouselArquitectura'/>
+            <div className={`portadaServiciosObras portadaArquitectura ${ !isIphone && 'bgfixed'}`}></div>
           </div>
-          <CarouselServicio imagenes={imgArquitectura} id='carouselArquitectura'/>
-          <div className={`portadaServiciosObras portadaArquitectura ${ !isIphone && 'bgfixed'}`}></div>
-        </div>
-        <div className='itemServicio' id='civilServicio'>
-          <div className='infoItemServicio'>
-            <div className='textoItemServicio'>
-              <h2>INGENIERÍA CIVIL</h2>
-              <ul>
-                <li>Redes de agua potable y cloaca.</li>
-                <li>Infraestructura vial, pavimentos de hormigón rígidos y flexibles, y pisos industriales.</li>
-                <li>Infraestructura de servicios, plazas, parques, equipamiento urbano, cordón cuneta, veredas y caminarías.</li>
-                <li>Construcción e instalación de plantas de tratamiento de efluentes cloacales.</li>
-                <li>Canalizaciones pluviales.</li>
-              </ul>
+        </LazyLoadComponent>
+        <LazyLoadComponent>
+          <div className='itemServicio' id='civilServicio'>
+            <div className='infoItemServicio'>
+              <div className='textoItemServicio'>
+                <h2>INGENIERÍA CIVIL</h2>
+                <ul>
+                  <li>Redes de agua potable y cloaca.</li>
+                  <li>Infraestructura vial, pavimentos de hormigón rígidos y flexibles, y pisos industriales.</li>
+                  <li>Infraestructura de servicios, plazas, parques, equipamiento urbano, cordón cuneta, veredas y caminarías.</li>
+                  <li>Construcción e instalación de plantas de tratamiento de efluentes cloacales.</li>
+                  <li>Canalizaciones pluviales.</li>
+                </ul>
+              </div>
+              <CarouselServicio imagenes={imgCivilXL} id='carouselCivilXL' desktop/>
             </div>
-            <CarouselServicio imagenes={imgCivilXL} id='carouselCivilXL' desktop/>
+            <CarouselServicio imagenes={imgCivil} id='carouselCivil'/>
+            <div className={`portadaServiciosObras portadaCivil ${ !isIphone && 'bgfixed'}`}></div>
           </div>
-          <CarouselServicio imagenes={imgCivil} id='carouselCivil'/>
-          <div className={`portadaServiciosObras portadaCivil ${ !isIphone && 'bgfixed'}`}></div>
-        </div>
-        <div className='itemServicio' id='electromecanicaServicio'>
-          <div className='infoItemServicio'>
-            <div className='textoItemServicio'>
-              <h2>ELECTROMECÁNICA</h2>
-              <ul>
-                <li>Alumbrado público.</li>
-                <li>Tendido de líneas aéreas y subterráneas de media y baja tensión.</li>
-                <li>Estaciones transformadoras de hasta 132 KV.</li>
-                <li>Montaje de subestaciones transformadoras.</li>
-                <li>Centros de distribución de 13.2 y 33 KV.</li>
-                <li>Instalación eléctrica para luz y fuerza motriz de edificios.</li>
-                <li>Iluminación deportiva.</li>
-                <li>Electrificación rural.</li>
-              </ul>
+        </LazyLoadComponent>
+        <LazyLoadComponent>
+          <div className='itemServicio' id='electromecanicaServicio'>
+            <div className='infoItemServicio'>
+              <div className='textoItemServicio'>
+                <h2>ELECTROMECÁNICA</h2>
+                <ul>
+                  <li>Alumbrado público.</li>
+                  <li>Tendido de líneas aéreas y subterráneas de media y baja tensión.</li>
+                  <li>Estaciones transformadoras de hasta 132 KV.</li>
+                  <li>Montaje de subestaciones transformadoras.</li>
+                  <li>Centros de distribución de 13.2 y 33 KV.</li>
+                  <li>Instalación eléctrica para luz y fuerza motriz de edificios.</li>
+                  <li>Iluminación deportiva.</li>
+                  <li>Electrificación rural.</li>
+                </ul>
+              </div>
+              <CarouselServicio imagenes={imgElectromecanicaXL} id='carouselElectromecanicaXL' desktop/>
             </div>
-            <CarouselServicio imagenes={imgElectromecanicaXL} id='carouselElectromecanicaXL' desktop/>
+            <CarouselServicio imagenes={imgElectromecanica} id='carouselElectromecanica'/>
+            <div className={`portadaServiciosObras portadaElectromecanica ${ !isIphone && 'bgfixed'}`}></div>
           </div>
-          <CarouselServicio imagenes={imgElectromecanica} id='carouselElectromecanica'/>
-          <div className={`portadaServiciosObras portadaElectromecanica ${ !isIphone && 'bgfixed'}`}></div>
-        </div>
-        <div className='itemServicio' id='metalurgicaServicio'>
-          <div className='infoItemServicio'>
-            <div className='textoItemServicio'>
-              <h2>METALÚRGICA</h2>
-              <ul>
-                <li>Naves industriales de distintos tipos y luces</li>
-                <li>Estructuras metálicas de cubierta.</li>
-                <li>Talleres de carpintería de chapa y aluminio. Especiales y estándar.</li>
-                <li>Accesorios para obras: andamios, moldes, rejas, escaleras, metálicas, barandas, tapas de cámara, etc.</li>
-              </ul>
+        </LazyLoadComponent>
+        <LazyLoadComponent>
+          <div className='itemServicio' id='metalurgicaServicio'>
+            <div className='infoItemServicio'>
+              <div className='textoItemServicio'>
+                <h2>METALÚRGICA</h2>
+                <ul>
+                  <li>Naves industriales de distintos tipos y luces</li>
+                  <li>Estructuras metálicas de cubierta.</li>
+                  <li>Talleres de carpintería de chapa y aluminio. Especiales y estándar.</li>
+                  <li>Accesorios para obras: andamios, moldes, rejas, escaleras, metálicas, barandas, tapas de cámara, etc.</li>
+                </ul>
+              </div>
+              <CarouselServicio imagenes={imgMetalurgicaXL} id='carouselMetalurgicaXL' desktop/>
             </div>
-            <CarouselServicio imagenes={imgMetalurgicaXL} id='carouselMetalurgicaXL' desktop/>
+            <CarouselServicio imagenes={imgMetalurgica} id='carouselMetalurgica'/>
+            <div className={`portadaServiciosObras portadaMetalurgica ${ !isIphone && 'bgfixed'}`}></div>
           </div>
-          <CarouselServicio imagenes={imgMetalurgica} id='carouselMetalurgica'/>
-          <div className={`portadaServiciosObras portadaMetalurgica ${ !isIphone && 'bgfixed'}`}></div>
-        </div>
+        </LazyLoadComponent>
       </div>
     </>
   )
